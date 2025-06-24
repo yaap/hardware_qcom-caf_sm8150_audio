15531c2bf0a85e27a14ad0d7c3a4b8b40c190778
Ido Ben-Hur <idoybh2@gmail.com>
2025-06-24 09:03:57 +0300

    spkr_protection: Adapt to A16

--- hal/audio_extn/spkr_protection.c
+++ hal/audio_extn/spkr_protection.c
@@ -1138,7 +1138,7 @@ static void spkr_calibrate_wait()
     pthread_mutex_unlock(&handle.cal_wait_cond_mutex);
 }
 
-static void* spkr_calibration_thread()
+static void* spkr_calibration_thread(void *arg)
 {
     unsigned long sec = 0;
     int t0;
@@ -1915,7 +1915,7 @@ static int get_spkr_prot_v_vali_param(int cal_fd __unused, int *status __unused,
 }
 #endif
 
-static void* spkr_v_vali_thread()
+static void* spkr_v_vali_thread(void *arg)
 {
     int ret = 0;
     struct audio_device *adev = handle.adev_handle;
